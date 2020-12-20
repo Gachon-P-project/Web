2a41239ad62d1575c2b8b7c94e4555b634a23379
Hyoeun Yoo <yuuhe0913@gmail.com>
2020-12-20 20:46:53 +0900

    update login page

--- moga-web/src/index.js
+++ moga-web/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './login';
+import App from './App';
 
 ReactDOM.render(
-    <Login />,
+    <App />,
   document.getElementById('root')
 );
